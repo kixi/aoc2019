1c49c437712167452a151d16ece155fa60bcc197
Kixi <4116520+kixi@users.noreply.github.com>
2019-12-08 09:51:44 +0100

    day 8 improvements

--- src/day08.clj
+++ src/day08.clj
@@ -1,22 +1,20 @@
 (ns day08
   (:require [clojure.java.io :as io]
-            [clojure.core.matrix :as mat]))
+            [clojure.core.matrix :as mat]
+            [clojure.string :as str]))
 
-(defn read-program []
-  (slurp (io/resource "day08.txt")))
+(def input (slurp (io/resource "day08.txt")))
 
 (defn decode [str]
   (->> str
        (map #(- (int %) (int \0)))
        (partition (* 25 6))))
 
-(def p (read-program))
-
 (defn m12 [x]
   (* (get x 1) (get x 2)))
 
 (def part1
-  (->> p
+  (->> input
        decode
        (map frequencies)
        (sort (fn [a b] (compare (get a 0) (get b 0))))
@@ -24,7 +22,7 @@
        m12))
 
 (defn visibility [channels]
-  (first (drop-while #(= % 2) channels)))
+  (first (drop-while (partial = 2) channels)))
 
 (defn draw-pixel [p]
   (get {0 " " 1 "*"} p))
@@ -33,18 +31,17 @@
   (map draw-pixel l))
 
 (defn draw [pic]
-  (doseq [line pic]
-    (doseq [p line]
-      (print p))
-    (println)))
+  (->>
+   pic
+   (map #(apply str %))
+   (str/join "\n")))
 
 (def part2
-  (->> p
+  (->> input
        decode
        mat/transpose
        (map visibility)
        (map draw-pixel)
        (partition 25)
-       draw))
-
-
+       draw
+       println))
